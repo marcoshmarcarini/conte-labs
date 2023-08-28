594dee67be5b9abf888ed3856529954341d1d2a3
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2023-08-28 10:48:00 -0300

    Update index.jsx

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -10,7 +10,7 @@ export default function login(){
     const clientId = process.env.CLIENT_ID
     const clientSecret = process.env.CLIENT_SECRET
     const igToken = process.env.IG_TOKEN
-    const redirectUrl = 'http://localhost:3000/metapage'
+    const redirectUrl = 'https://conte-labs.vercel.app/metapage'
     const scope = 'user_profile'
     const state = '1'
     const url = `https://api.instagram.com/oauth/authorize?client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUrl}&scope=${scope}&response_type=code&state=${state}`
